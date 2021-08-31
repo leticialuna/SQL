fd57031d815ab82029b46a3292d4097d8386c495
leticialuna <62301027+leticialuna@users.noreply.github.com>
2021-08-31 12:07:24 +0200

    average population of each continent - inner join

---
+++ HackerrankPractice/Basic/Average Population of Each Continent.sql
@@ -0,0 +1,6 @@
+-- # https://www.hackerrank.com/challenges/average-population-of-each-continent/problem
+
+select country.continent, floor(avg(city.population))
+from city inner join country
+on city.countrycode = country.code
+group by country.continent;
