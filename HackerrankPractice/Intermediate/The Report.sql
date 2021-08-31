7e4cbc83919be392dbf71336958ae89b57113aca
leticialuna <62301027+leticialuna@users.noreply.github.com>
2021-08-31 13:49:47 +0200

    the report.sql - select if and join

---
+++ HackerrankPractice/Intermediate/The Report.sql
@@ -0,0 +1,6 @@
+-- # https://www.hackerrank.com/challenges/the-report/problem
+
+select if(grade < 8, null, name), grade, marks
+from students join grades
+where marks between min_mark and max_mark
+order by grade desc, name;
