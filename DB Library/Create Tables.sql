d32775bc2b773cc2460303368a450eb9b1c295b7
leticialuna <62301027+leticialuna@users.noreply.github.com>
2021-10-18 15:33:45 +0200

    create table authors, publishers and books

--- DB Library/Create Tables.sql
+++ DB Library/Create Tables.sql
@@ -4,12 +4,12 @@ CREATE DATABASE sql_library;
 
 -- Create Tables 
 
--- Create Table Autors:
+-- Create Table Authors:
 
-CREATE TABLE autors (
-id_autor int NOT NULL AUTO_INCREMENT,  
-autor_name varchar(45) NOT NULL, 
-autor_surname varchar(45),
+CREATE TABLE authors (
+id_author int NOT NULL AUTO_INCREMENT,  
+author_name varchar(45) NOT NULL, 
+author_surname varchar(45),
 PRIMARY KEY (id_publisher) 
 );
 
@@ -26,15 +26,15 @@ PRIMARY KEY (id_publisher)
 CREATE TABLE books (
 id_book INT NOT NULL AUTO_INCREMENT,
 book_name varchar(70) NOT NULL,
-id_autor INT NULL,
+id_author INT NULL,
 id_publisher INT NULL,
 book_pages INT NULL,
 PRIMARY KEY (id_book),
-INDEX fk_id_autor_idx (id_autor ASC),
+INDEX fk_id_author_idx (id_author ASC),
 INDEX fk_id_publisher_idx (id_publisher ASC),
-CONSTRAINT fk_id_autor
-	  FOREIGN KEY (id_autor)
-    REFERENCES sql_library.autors (id_autor)
+CONSTRAINT fk_id_author
+	  FOREIGN KEY (id_author)
+    REFERENCES sql_library.authors (id_author)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
 CONSTRAINT fk_id_publisher
